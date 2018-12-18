c8d7e8ec8423d28aa1381bd173fed5c9185af599
hiraaki0109 <sendaitobuhudosan@gmail.com>
2018-12-18 17:05:46 +0900

    Initial Commit

---
+++ Instagram/HomeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewController.swift
+//  Instagram
+//
+//  Created by USER on 2018/11/17.
+//  Copyright © 2018年 仙台東部不動産. All rights reserved.
+//
+
+import Foundation
---
+++ Instagram/const.swift
@@ -0,0 +1,14 @@
+
+//
+//  const.swift
+//  Instagram
+//
+//  Created by USER on 2018/12/04.
+//  Copyright © 2018年 仙台東部不動産. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
